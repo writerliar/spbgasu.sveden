07e2f3c26227be1202209133c197ccd4f6a5447a
writerliar <kovach.dana@yandex.ru>
2018-11-30 14:37:17 +0300

    Реализация специального плагина ОВЗ

--- omg_ovz/sveden/update/read_settings.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function tempCopyFile($fromDir, $delDir, $tempDir)
-{
-    copy($fromDir . '/index.html', $tempDir. '/index.html');
-    removeDirectory($delDir);
-    if (!file_exists($fromDir)) mkdir($fromDir);
-    copy($tempDir . '/index.html', $fromDir. '/index.html');
-}
